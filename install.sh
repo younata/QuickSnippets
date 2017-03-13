a4dcb3a7bce86a89daffc0b8f634f9e0ae8676dd
Rachel Brindle <you@subluminal.net>
2017-03-13 11:00:26 -0700

    Ensure codesnippets directory exists
    Apparently Xcode only adds this directory upon demand

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Installing Quick Code Snippets"
+mkdir -p ~/Library/Developer/Xcode/UserData/CodeSnippets/
 cp CodeSnippets/* ~/Library/Developer/Xcode/UserData/CodeSnippets/
 if [ "$?" -eq "0" ]; then
     echo "Success"
